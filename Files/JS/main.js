89a94d1d72d2245e070bd6c64159927e9c6d9e95
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-10-19 09:40:02 +0330

    Create menu

--- Files/JS/main.js
+++ Files/JS/main.js
@@ -1,3 +1,4 @@
+console.log("Connected...")
 function Write_time()
 {
     const Time = document.querySelector(".Time")
@@ -8,3 +9,50 @@ function Write_time()
     Time.innerHTML = sec + " : " + min + " : " + hou
 }
 window.setInterval(Write_time, 1000)
+function start_menu()
+{
+    var id = null;
+    let circle = document.getElementById("circle");
+    let item1 = document.getElementById("item1")
+    let item2 = document.getElementById("item2")
+    var pos = 600;
+    circle.style.visibility = "visible";
+    id = setInterval(frame, 10);
+    function frame() 
+    {
+        // first list item: 650
+        // second list item: 725
+        if (pos == 800) 
+        {
+            circle.style.visibility = 'hidden';
+            clearInterval(id);
+        } 
+        else 
+        {
+            if (pos == 650)
+            {
+                if (item1.style.visibility == 'hidden') 
+                {
+                    item1.style.visibility = 'visible';
+                }
+                else 
+                {
+                    item1.style.visibility = 'hidden';
+                }
+            }
+            else if (pos == 725)
+            {
+                if (item2.style.visibility == 'hidden') 
+                {
+                    item2.style.visibility = 'visible';
+                }
+                else 
+                {
+                    item2.style.visibility = 'hidden';
+                }
+            }
+            pos += 2.5;
+            circle.style.right = pos + 'px'; 
+        }
+    }
+}
