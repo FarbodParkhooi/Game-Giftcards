a68803926916b03a9b3a42a64716bb1f36daa112
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-10-19 15:19:47 +0330

    menu v2

--- Files/JS/main.js
+++ Files/JS/main.js
@@ -1,4 +1,5 @@
 console.log("Connected...")
+var open = false;
 function Write_time()
 {
     const Time = document.querySelector(".Time")
@@ -11,49 +12,16 @@ function Write_time()
 window.setInterval(Write_time, 1000)
 function start_menu()
 {
-    var id = null;
-    let circle = document.getElementById("circle");
-    let item1 = document.getElementById("item1")
-    let item2 = document.getElementById("item2")
-    var pos = 600;
-    clearInterval(id);
-    circle.style.visibility = "visible";
-    id = setInterval(frame, 10);
-    function frame() 
+    let menu = document.getElementById("Menu-con")
+    let btn = document.getElementById("menu-btn")
+    if (open == false)
     {
-        // first list item: 650
-        // second list item: 725
-        if (pos == 800) 
-        {
-            clearInterval(id);
-            circle.style.visibility = 'hidden';
-        } 
-        else 
-        {
-            if (pos == 650)
-            {
-                if (item1.style.visibility == 'hidden') 
-                {
-                    item1.style.visibility = 'visible';
-                }
-                else 
-                {
-                    item1.style.visibility = 'hidden';
-                }
-            }
-            else if (pos == 725)
-            {
-                if (item2.style.visibility == 'hidden') 
-                {
-                    item2.style.visibility = 'visible';
-                }
-                else 
-                {
-                    item2.style.visibility = 'hidden';
-                }
-            }
-            pos += 2.5;
-            circle.style.right = pos + 'px'; 
-        }
+        menu.style.left = "-80px";
+        open = true;
+    }
+    else if (open == true)
+    {
+        menu.style.left = "-300px";
+        open = false;
     }
 }
